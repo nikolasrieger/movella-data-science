0c349c4aefac848e829a3d1ebbd1d1cdfd19f704
nikolas.rieger <nikolas.rieger@student.hpi.de>
2024-12-09 13:33:18 +0100

    feat: fix data merger

--- data.py
+++ data.py
@@ -1,32 +1,18 @@
 import pandas as pd
 
-def clean_csv(in_name, out_name):
-    df1 = pd.read_csv(in_name, skiprows=11) 
-    df1.to_csv(out_name, index=False)
+def clean_csv(input_file, output_file, skiprows=11):
+    df1 = pd.read_csv(input_file, skiprows=skiprows) 
+    df1.to_csv(output_file, index=False)
+    print(f"Merged file saved to: {output_file}")
 
-def merge_csv(in_name1, in_name2, out_name="merged.csv"):
-    df1 = pd.read_csv(in_name1)
-    df2 = pd.read_csv(in_name2)
+def merge_csv(input_file1, input_file2, output_file="merged.csv"):
+    df1 = pd.read_csv(input_file1)
+    df2 = pd.read_csv(input_file2)
     merged_df = pd.merge(df1, df2, on='PacketCounter', how='inner')
-    merged_df.to_csv(out_name, index=False)
+    merged_df.to_csv(output_file, index=False)
+    print(f"Merged file saved to: {output_file}")
 
 clean_csv('rf2.csv', 'rf2_clean.csv')
 clean_csv('rf3.csv', 'rf3_clean.csv')
 
-csv_file1 = 'rf2_clean.csv'
-csv_file2 = 'rf3_clean.csv'
-
-# Output file
-output_file = 'merged.csv'
-
-# Read the CSV files
-df1 = pd.read_csv(csv_file1)
-df2 = pd.read_csv(csv_file2)
-
-# Join the two DataFrames on the 'PacketCounter' column
-merged_df = pd.merge(df1, df2, on='PacketCounter', how='inner')  # Use 'how' as 'inner' for matching rows only
-
-# Save the merged DataFrame to a new CSV file
-merged_df.to_csv(output_file, index=False)
-
-print(f"Merged file saved to: {output_file}")
+merge_csv('rf2_clean.csv', 'rf3_clean.csv', 'merged.csv')
